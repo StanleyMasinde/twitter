a3c8afc598b6246088016c7776e45c39890c6786
Stanley Masinde <stanmasinde@gmail.com>
2025-09-18 03:14:05 +0300

    feat: add support for threads
    This commit adds intial support for tweeting threads.
    
    The idea is to separate tweets in a thread with ---
    
    If the dashes are seen, the tweet logic will loop through the tweets one
    after another.
    
    1. Send the first tweet.
    2. Wait for the response and extract the tweet id
    3. Use that id to send the next tweet as a reply to that.
    
    The current state is rough and just POC. The codebase is very dirty.
    
    ------
    This feature is supported both in cli mode and server mode.

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,5 +1,8 @@
+use std::fmt::Display;
+
 use log::info;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 
 pub trait HttpClient {
     fn new() -> Self;
@@ -10,24 +13,48 @@ pub trait HttpClient {
     fn post(
         &self,
         url: &str,
-        body: serde_json::Value,
     ) -> impl std::future::Future<Output = Result<Response, reqwest::Error>> + Send;
+    fn json<T: Serialize + ?Sized>(&mut self, json: &T) -> &Self;
 }
 
 pub struct Response {
     pub status: u16,
-    pub content: String,
+    pub content: TweetCreateResponse,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TweetCreateResponse {
+    pub data: TweetData,
+}
+
+impl Display for TweetCreateResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Tweet Id: {}\nTweet body: {}",
+            self.data.id, self.data.text
+        )
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TweetData {
+    pub text: String,
+    pub edit_history_tweet_ids: Vec<String>,
+    pub id: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct ApiClient {
     client: reqwest::Client,
     bearer_token: String,
+    body: Option<Vec<u8>>,
+    content_type: Option<String>,
 }
 
 impl ApiClient {
-    pub fn with_bearer(&mut self, token: &str) -> &Self {
-        self.bearer_token = token.to_string();
+    pub fn with_bearer(&mut self, token: String) -> &Self {
+        self.bearer_token = token;
 
         self
     }
@@ -40,6 +67,8 @@ impl HttpClient for ApiClient {
         Self {
             client,
             bearer_token,
+            body: None,
+            content_type: None,
         }
     }
 
@@ -49,21 +78,40 @@ impl HttpClient for ApiClient {
         Ok(res)
     }
 
-    async fn post(&self, url: &str, body: serde_json::Value) -> Result<Response, reqwest::Error> {
+    async fn post(&self, url: &str) -> Result<Response, reqwest::Error> {
         info!("foo: {}", self.bearer_token);
         let res = self
             .client
             .post(url)
             .header(reqwest::header::AUTHORIZATION, &self.bearer_token)
-            .json(&body)
+            .json(&self.body)
             .send()
             .await?;
 
+        let status = res.status();
+
+        let content = res.json().await?;
+
         let response = Response {
-            status: res.status().into(),
-            content: res.text().await?,
+            status: status.into(),
+            content,
         };
         Ok(response)
+    }
+
+    fn json<T: Serialize + ?Sized>(&mut self, json: &T) -> &Self {
+        match serde_json::to_vec(json) {
+            Ok(data) => {
+                self.body = Some(data);
+                self.content_type = Some("application/json".into());
+            }
+            Err(err) => {
+                // up to you: either panic, return Result<Self>, or store the error
+                panic!("Failed to serialize body: {err}");
+            }
+        }
+
+        self
     }
 }
 
@@ -84,12 +132,13 @@ mod test {
                 Ok(format!("GET {url}").to_string())
             }
 
-            async fn post(
-                &self,
-                url: &str,
-                _body: serde_json::Value,
-            ) -> Result<Response, reqwest::Error> {
-                let res = format!("POST {url}").to_string();
+            async fn post(&self, _url: &str) -> Result<Response, reqwest::Error> {
+                let data = TweetData {
+                    text: "fooo".to_string(),
+                    edit_history_tweet_ids: vec![],
+                    id: 0.to_string(),
+                };
+                let res = TweetCreateResponse { data };
 
                 let response = Response {
                     content: res,
@@ -97,6 +146,10 @@ mod test {
                 };
 
                 Ok(response)
+            }
+
+            fn json<T: Serialize + ?Sized>(&mut self, _json: &T) -> &Self {
+                self
             }
         }
 
--- src/twitter/tweet.rs
+++ src/twitter/tweet.rs
@@ -1,28 +1,192 @@
 use crate::{
-    api::client::{HttpClient, Response},
+    api::client::{Response, TweetCreateResponse, TweetData},
     config::Config,
 };
 use oauth::{HMAC_SHA1, Token};
 use reqwest::Error;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
 
-use crate::{api::client::ApiClient, server::routes::api::CreateTweet};
+#[derive(Serialize, Deserialize)]
+pub struct TweetBody {
+    pub text: Option<String>,
+    pub reply: Option<Reply>,
+}
 
-pub async fn create(mut client: ApiClient, payload: CreateTweet) -> Result<Response, Error> {
-    let cfg = Config::load();
+#[derive(Serialize, Deserialize)]
+pub struct Reply {
+    pub in_reply_to_tweet_id: String,
+}
 
-    let token = Token::from_parts(
-        cfg.consumer_key,
-        cfg.consumer_secret,
-        cfg.access_token,
-        cfg.access_secret,
-    );
+pub trait TwitterApi {
+    async fn create(&mut self) -> Result<Response, Error>;
+}
 
-    let url = "https://api.twitter.com/2/tweets";
-    let auth_header = oauth::post(url, &(), &token, HMAC_SHA1);
+pub struct Tweet {
+    client: reqwest::Client,
+    previous_tweet: Option<String>,
+    separator: String,
+    payload: TweetBody,
+    tweet_parts: Vec<String>,
+}
 
-    client
-        .with_bearer(&auth_header)
-        .post(url, json!({ "text": payload.text }))
-        .await
+impl Default for Tweet {
+    fn default() -> Self {
+        let reply = Reply {
+            in_reply_to_tweet_id: 0.to_string(),
+        };
+
+        let payload = TweetBody {
+            text: Some(String::new()),
+            reply: Some(reply),
+        };
+
+        Self {
+            client: Default::default(),
+            previous_tweet: Default::default(),
+            separator: Default::default(),
+            payload,
+            tweet_parts: Default::default(),
+        }
+    }
+}
+
+impl Tweet {
+    pub fn new(client: reqwest::Client, payload: TweetBody) -> Self {
+        Self {
+            client,
+            previous_tweet: None,
+            separator: "---".to_string(),
+            payload,
+            tweet_parts: vec![],
+        }
+    }
+
+    fn is_thread(&self, tweet: &String) -> bool {
+        tweet.lines().any(|line| line.trim() == self.separator)
+    }
+
+    fn split_tweet(&self, tweet: &String, separator: &str) -> Vec<String> {
+        tweet
+            .lines()
+            .collect::<Vec<&str>>()
+            .split(|line| line.trim() == separator)
+            .map(|chuck| chuck.join("\n").trim().to_string())
+            .collect()
+    }
+
+    async fn send(&mut self, index: Option<usize>) -> Result<TweetCreateResponse, Error> {
+        let cfg = Config::load();
+        let token = Token::from_parts(
+            cfg.consumer_key,
+            cfg.consumer_secret,
+            cfg.access_token,
+            cfg.access_secret,
+        );
+        let url = "https://api.twitter.com/2/tweets";
+        let auth_header = oauth::post(url, &(), &token, HMAC_SHA1);
+        let mut reply = None;
+        if self.previous_tweet.is_some() {
+            reply = Some(Reply {
+                in_reply_to_tweet_id: self.previous_tweet.clone().unwrap(),
+            });
+        }
+
+        let mut tweet_text: String = self.payload.text.clone().unwrap();
+
+        if let Some(current_index) = index {
+            tweet_text = self
+                .tweet_parts
+                .get(current_index)
+                .map_or("", |v| v)
+                .to_string();
+        }
+
+        let new_tweet = TweetBody {
+            text: Some(tweet_text),
+            reply,
+        };
+
+        let response = self
+            .client
+            .post(url)
+            .header(reqwest::header::AUTHORIZATION, &auth_header)
+            .json(&new_tweet)
+            .send()
+            .await?
+            .json::<TweetCreateResponse>()
+            .await?;
+
+        Ok(response)
+    }
+}
+
+impl TwitterApi for Tweet {
+    async fn create(&mut self) -> Result<Response, Error> {
+        let text = self.payload.text.clone().unwrap();
+        let tweet_data = TweetData {
+            text: "".to_string(),
+            edit_history_tweet_ids: vec![],
+            id: 0.to_string(),
+        };
+        let content = TweetCreateResponse { data: tweet_data };
+        let response = Response {
+            status: 200,
+            content,
+        };
+
+        if self.is_thread(&text) {
+            let parts = self.split_tweet(&text, &self.separator);
+            self.tweet_parts = parts.clone();
+            for index in 0..parts.len() {
+                let res = self.send(Some(index)).await;
+
+                if let Ok(response) = res {
+                    self.previous_tweet = Some(response.data.id);
+                }
+            }
+        } else {
+            self.send(None).await?;
+        }
+
+        Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_tweet() {
+        let thread = r#"This is an awesome thread.
+        We be talking about things.
+        ---
+        This should be the second part.
+        "#
+        .to_string();
+
+        let tweet = Tweet::default();
+
+        let tweets = tweet.split_tweet(&thread, "---");
+
+        assert_eq!(tweets.len(), 2);
+    }
+
+    #[test]
+    fn test_is_thread() {
+        let thread = r#"This is an awesome thread.
+        We be talking about things.
+        ---
+        This should be the second part.
+        "#
+        .to_string();
+
+        let tweet_2 = Tweet::default();
+
+        assert!(tweet_2.is_thread(&thread));
+
+        let tweet = "This is a normal tweet.".to_string();
+
+        assert!(!tweet_2.is_thread(&tweet))
+    }
 }
