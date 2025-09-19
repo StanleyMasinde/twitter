6c0b5282db8b7ffe56375540e16cf840f2bffce8
Stanley Masinde <stanmasinde@gmail.com>
2025-09-20 01:33:49 +0300

    chore: Delete the custom http client
    It is just adding complications

--- src/twitter/mod.rs
+++ src/twitter/mod.rs
@@ -1,1 +1,32 @@
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
+
 pub mod tweet;
+
+pub struct Response<T> {
+    pub status: u16,
+    pub content: T,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TweetCreateResponse {
+    pub data: TweetData,
+}
+
+impl Display for TweetCreateResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Tweet Id: {}\nTweet body: {}",
+            self.data.id, self.data.text
+        )
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TweetData {
+    pub text: String,
+    pub edit_history_tweet_ids: Vec<String>,
+    pub id: String,
+}
