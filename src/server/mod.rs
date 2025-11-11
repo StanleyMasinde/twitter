7c2273050627eef43f9065f078e0792e235d556d
Stanley Masinde <stanmasinde@gmail.com>
2025-11-12 00:16:33 +0300

    Revert "feat: add a proper 404 endpoing handler."
    This reverts commit 6907a2e35af7871238d1ce25a681b4d73397dd59.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,21 +3,13 @@ pub mod routes;
 use std::io::ErrorKind;
 
 use axum::Router;
-use reqwest::StatusCode;
 
 pub async fn run(port_param: Option<u16>) {
     let default_port: u16 = 3000;
     let port = port_param.unwrap_or(default_port);
     let address = format!("127.0.0.1:{port}");
 
-    let app = Router::new()
-        .nest("/api", routes::api::api_routes())
-        .fallback(async || {
-            (
-                StatusCode::NOT_FOUND,
-                "Route not found.",
-            )
-        });
+    let app = Router::new().nest("/api", routes::api::api_routes());
 
     let listener = match tokio::net::TcpListener::bind(address)
         .await
